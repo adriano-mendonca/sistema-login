6e444fbc8be261d816b968e03c5e4ea89f33f12c
Adriano Brito de Mendonça <adrianoskiazy51@gmail.com>
2024-06-11 17:52:45 -0300

    Começo da implementação da tabela

--- src/Components/Contas/Contas.jsx
+++ src/Components/Contas/Contas.jsx
@@ -5,19 +5,24 @@ import { CONTA_GET } from "../../Api";
 
 export const Contas = () => {
   const { data } = React.useContext(UserContext);
-  const [contas, setContas] = React.useState(null)
+  const [contas, setContas] = React.useState(null);
+  const dataContas = React.useMemo(() => contas, [])
+  const columns = React.useMemo(() => [])
 
-  React.useContext(() => {
+
+  React.useEffect(() => {
     async function getContas() {
       const token = window.localStorage.getItem("token");
-      if(token) {
-        const {url, options} = CONTA_GET(token)
-        const response = await fetch(url, options)
-        const json = await response.json()
+      if (token) {
+        const { url, options } = CONTA_GET(token);
+        const response = await fetch(url, options);
+        const json = await response.json();
         console.log(json)
+        setContas(json);
       }
     }
-    getContas()
-  },[])
-  return <div>teste</div>;
+    getContas();
+  }, []);
+
+  return <div>teste 123</div>;
 };
